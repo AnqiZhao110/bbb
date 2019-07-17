6c8c558789195527e2f4c2f87513cec8ea474efa
zhaoanqi <zhaoanqi_lee@163.com>
2019-07-17 16:13:13 +0800

    master init 01

---
+++ src/main/java/cn/aura/servletDemo/Userservlet.java
@@ -0,0 +1,25 @@
+package cn.aura.servletDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Userservlet
+ */
+@WebServlet("/user")
+public class Userservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("接收到请求");
+		PrintWriter writer = response.getWriter();
+		writer.println("success");
+		writer.close();
+	}
+
+}
